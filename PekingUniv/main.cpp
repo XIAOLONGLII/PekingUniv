01ceb23d77072c7b66792847ed3295689c09da32
XIAOLONG LI <1491skdpf89@gmail.com>
2018-12-23 12:46:47 -0500

    Update main.cpp
    This is a online course from Coursera.

--- PekingUniv/main.cpp
+++ PekingUniv/main.cpp
@@ -7,9 +7,26 @@
 //
 
 #include <iostream>
+using namespace std;
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+    
+    //2
+    float a = 0, b = 0, c = 0; // 初始值
+    cout << "Enter two numbers: ";
+    cin >> a >> b;
+    cout << "a = " << a << ",b = "<< b << endl;
+    c = a;
+    a = b;
+    b = c;
+    cout << "a = " << a << ",b = "<< b << endl; // 输出到屏幕上 // endl-》换行
+    
+    // 3
+    int x = 0, y = 0;
+    cin >> x >> y;
+    if( x > y) //判断语句
+        cout << "max = " << x << endl;
+    else
+        cout << "max = " << y << endl;
+    
 }
