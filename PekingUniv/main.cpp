011c63f8d334c9e028e5f33ba9b2528daee0f9ca
XIAOLONG LI <1491skdpf89@gmail.com>
2018-12-23 13:38:31 -0500

    All odd numbers smaller 20

--- PekingUniv/main.cpp
+++ PekingUniv/main.cpp
@@ -12,6 +12,7 @@ using namespace std;
 int main(int argc, const char * argv[]) {
     
     //2
+    cout << "#2" <<endl;
     float a = 0, b = 0, c = 0; // 初始值
     cout << "Enter two numbers: ";
     cin >> a >> b;
@@ -22,11 +23,39 @@ int main(int argc, const char * argv[]) {
     cout << "a = " << a << ",b = "<< b << endl; // 输出到屏幕上 // endl-》换行
     
     // 3
+    cout << "#3" <<endl;
     int x = 0, y = 0;
     cin >> x >> y;
     if( x > y) //判断语句
         cout << "max = " << x << endl;
     else
         cout << "max = " << y << endl;
+    
+    // 4
+    // == -> equal or not
+    //= -> give value to a variable
+    cout << "#4" <<endl;
+    char d = 'A';
+    cout << "Guess what is me? from A-Z"<<endl;
+    cin >> d;
+//    if( d != 'G')
+//       cout << " Wrong answer."<<endl;
+//    else if(a == 'G')
+//       cout << "Bingo, you are right." << endl;
+    
+    while( d != 'A')
+    {
+        cout << "wrong";
+        cin >> d;
+    }
+    cout << " Binggo you are right!" <<endl;
+    
+    // 5
+    cout << " Odd number in 20 "<<endl;
+    for (int i = 0; i < 20; i++)
+    {
+        if( i % 2 != 0)
+            cout << i << endl;
+    }
     
 }
