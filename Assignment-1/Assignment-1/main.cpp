ad95fac6696742a1c4a2222422dad9cd02279556
XIAOLONG LI <1491skdpf89@gmail.com>
2018-12-23 14:44:16 -0500

    Assignment-1
    Bubblesort

---
+++ Assignment-1/Assignment-1/main.cpp
@@ -0,0 +1,29 @@
+//
+//  main.cpp
+//  Assignment-1
+//
+//  Created by Xiaolong Li on 12/23/18.
+//  Copyright © 2018 Xiaolong Li. All rights reserved.
+//
+
+#include <iostream>
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+    
+    int n, a[1000]; // n integers, n is not more than 1000, a is a collection
+    
+    cin >> n;
+    for(int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+    
+    //冒泡，不断比较相邻的两个数，如果顺序出错（前大于后）， 就换位
+    
+    
+    
+    
+    
+    return 0;
+}
--- PekingUniv/main.cpp
+++ PekingUniv/main.cpp
@@ -6,72 +6,76 @@
 //  Copyright © 2018 Xiaolong Li. All rights reserved.
 //
 
+// 1. 正确性
+// 2. 可读性
+// 3. 美丽结构
+
 #include <iostream>
 using namespace std;
 
 int main(int argc, const char * argv[]) {
     
-//    //2
-//    cout << "#2" <<endl;
-//    float a = 0, b = 0, c = 0; // 初始值
-//    cout << "Enter two numbers: ";
-//    cin >> a >> b;
-//    cout << "a = " << a << ",b = "<< b << endl;
-//    c = a;
-//    a = b;
-//    b = c;
-//    cout << "a = " << a << ",b = "<< b << endl; // 输出到屏幕上 // endl-》换行
-//
-//    // 3
-//    cout << "#3" <<endl;
-//    int x = 0, y = 0;
-//    cin >> x >> y;
-//    if( x > y) //判断语句
-//        cout << "max = " << x << endl;
-//    else
-//        cout << "max = " << y << endl;
-//
-//    // 4
-//    // == -> equal or not
-//    //= -> give value to a variable
-//    cout << "#4" <<endl;
-//    char d = 'A';
-//    cout << "Guess what is me? from A-Z"<<endl;
-//    cin >> d;
-////    if( d != 'G')
-////       cout << " Wrong answer."<<endl;
-////    else if(a == 'G')
-////       cout << "Bingo, you are right." << endl;
-//
-//    while( d != 'A')
-//    {
-//        cout << "wrong";
-//        cin >> d;
-//    }
-//    cout << " Binggo you are right!" <<endl;
-//
-//    // 5
-//    cout << " Odd number in 20 "<<endl;
-//    for (int i = 0; i < 20; i++)
-//    {
-//        if( i % 2 != 0)
-//            cout << i << endl;
-//    }
-//
-//    for(int i = 20; i > 0; i--)
-//    {
-//        if( i % 2 == 0)
-//            cout << i << endl;
-//    }
-//    // 6
-//    char z[10] = { 'a','b','c','d','e','f','g','h','i','j'}; // store in ram
-//    for(int i = 0; i < 10; i++)
-//    {
-//        if( i % 2 != 0)
-//        {
-//            cout << z[i] << endl;
-//        }
-//    }
+    //2
+    cout << "#2" <<endl;
+    float a = 0, b = 0, c = 0; // 初始值
+    cout << "Enter two numbers: ";
+    cin >> a >> b;
+    cout << "a = " << a << ",b = "<< b << endl;
+    c = a;
+    a = b;
+    b = c;
+    cout << "a = " << a << ",b = "<< b << endl; // 输出到屏幕上 // endl-》换行
+
+    // 3
+    cout << "#3" <<endl;
+    int x = 0, y = 0;
+    cin >> x >> y;
+    if( x > y) //判断语句
+        cout << "max = " << x << endl;
+    else
+        cout << "max = " << y << endl;
+
+    // 4
+    // == -> equal or not
+    //= -> give value to a variable
+    cout << "#4" <<endl;
+    char d = 'A';
+    cout << "Guess what is me? from A-Z"<<endl;
+    cin >> d;
+//    if( d != 'G')
+//       cout << " Wrong answer."<<endl;
+//    else if(a == 'G')
+//       cout << "Bingo, you are right." << endl;
+
+    while( d != 'A')
+    {
+        cout << "wrong";
+        cin >> d;
+    }
+    cout << " Binggo you are right!" <<endl;
+
+    // 5
+    cout << " Odd number in 20 "<<endl;
+    for (int i = 0; i < 20; i++)
+    {
+        if( i % 2 != 0)
+            cout << i << endl;
+    }
+
+    for(int i = 20; i > 0; i--)
+    {
+        if( i % 2 == 0)
+            cout << i << endl;
+    }
+    // 6
+    char z[10] = { 'a','b','c','d','e','f','g','h','i','j'}; // store in ram
+    for(int i = 0; i < 10; i++)
+    {
+        if( i % 2 != 0)
+        {
+            cout << z[i] << endl;
+        }
+    }
     
     // 7: 注释:可读性一定保证，因为别人要读懂。重要因素
     //添加注释 comment， 排版
