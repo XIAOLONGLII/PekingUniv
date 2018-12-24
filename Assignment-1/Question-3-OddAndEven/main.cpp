e2158a983cb1ba5b4642cbf06a8f923f3b0afdd1
XIAOLONG LI <1491skdpf89@gmail.com>
2018-12-23 22:22:32 -0500

    BuubleSort
    Steps.

--- Assignment-1/Question-3-OddAndEven/main.cpp
+++ Assignment-1/Question-3-OddAndEven/main.cpp
@@ -22,6 +22,48 @@ int main(int argc, const char * argv[]) {
         for(int j = 1; j < 10 - i; j++)
         {
             
+            /*1.
+                 i = 0, j = 1 ... 10
+                 i = { 0...8}
+             j = { 1..9}
+                 arr[j - 1] > arr[j]
+                 j = 1, arr[ 0] > arr[1], swap
+                 j = 2, arr[1] > arr[2], swap
+                 j =3 , 4 5 6 7 8
+                 the biggest one will be at the last.
+                 after i = 0, it will be look like: 9,8,7,6,5,4,3,2,1,10
+             */
+            
+            /*2.
+                i = 1, j = 1....9
+                i = {1...8}
+                j = {1..8}
+                arr[j-1] = arr[0,1,2,3,4,5,6,7]
+             after i = 1; it will be: 8,7,6,5,4,3,2,1,9,10
+       
+             
+             
+             ...
+             
+             i = 8, it will be
+             1, 2, 3, 4, 5, 6, 7, 8, 9, 10
+             
+             
+            */
+            
+            
+            
+            // i = 1, j = 1...9
+            //{2,3,4,5,6,7,8,9}
+            //arr[
+            
+            
+            
+            
+            
+            // i = 2, j = 1...8
+            // i = 3, j = 1.
+            
             bool leftIsEven = arr[ j - 1] % 2 == 0;
             bool rightIsEven = arr[j] % 2 == 0;
             if((leftIsEven && !rightIsEven)||(leftIsEven == rightIsEven && arr[j - 1] > arr[j]))
