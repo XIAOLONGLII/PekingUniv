61fe6f631c218256339ed1035b529c57b98696c2
XIAOLONG LI <1491skdpf89@gmail.com>
2018-12-26 17:41:15 -0500

    Assignement3 -2, -3

--- Week-6/Assignment3-2/main.cpp
+++ Week-6/Assignment3-2/main.cpp
@@ -1,10 +1,36 @@
 //  Assignment3-2
-//
+//编程题＃1：苹果和虫子
+
+/*思考
+ 1。n个苹果
+ 2. n- y/x 苹果数
+ 3. if y%x ！= 0 y/x = y / x + 1；
+ 
+ 
+ */
 
 #include <iostream>
+using namespace std;
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+    
+    int n, x, y;
+    cout << "How many apples total:  and what is x and  y"<< endl;
+    cin >> n >> x >> y;
+    
+    int applesEat = y / x;
+    
+    if( y % x != 0)
+    {
+        applesEat++;
+    }
+    if (applesEat > n)
+    {
+        cout << 0;
+    }
+    else
+    {
+        cout << n - applesEat;
+    }
+    cout << endl;
 }
---
+++ Week-6/Assignment3-3/main.cpp
@@ -0,0 +1,30 @@
+//  Assignment3-3
+// 编程题＃2：大象喝水
+
+/*
+ 20升 = 20x 10^3毫升 = 20x 10^3 cm^3
+ cube = r^ 2 *  h
+ 
+ */
+
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+const double VOLUME = 20000;
+const double pi = 3.1415926535897;
+
+int main(int argc, const char * argv[])
+{
+    cout << "Enter r and h: "<< endl;
+    double r, h, volume;
+    cin >> r >> h;
+    volume = pi * pow(r,2) * h;
+    cout << volume << endl;
+    int num = VOLUME / volume;
+    cout << num << endl;
+    if (remainder(VOLUME, volume) != 0){
+        num++;
+    }
+    cout << num << endl;
+}
