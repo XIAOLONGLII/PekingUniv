991d7a5fb5f53f9711631a9196b3b82215a6e489
XIAOLONG LI <1491skdpf89@gmail.com>
2019-01-02 17:43:50 -0500

    Walk-Bike
    Key: compare time

--- Week-8/Assignment5-2/main.cpp
+++ Week-8/Assignment5-2/main.cpp
@@ -1,42 +1,44 @@
 //编程题＃2：骑车与走路
-
-
 #include <iostream>
 #include <vector>
 using namespace std;
 
 int main(int argc, const char * argv[])
 {
-    double open = 27, lock = 23, speed = 3.0;
+    double open = 27, lock = 23, bikeSpeed = 3.0;
     double walkSpeed = 1.2;
     
-    double c = 0 ;
-    c =  ( open + lock ) * 12 / 6;
-   //cout << "c= " << c << endl;
     int n = 0;
     //cout << "how many things need to do: ";
     cin >> n;
-    int distance = 0;
-    vector<int> my_vector;
+    double distance = 0;
+    vector<string> my_vector;
+    string status = "";
     for(int i = 0; i < n; i++)
     {
         cin >> distance;
-        my_vector.push_back(distance);
+        double time_bike = distance / bikeSpeed + open + lock;
+        double time_walk = distance / walkSpeed;
+        
+        if( time_bike > time_walk)
+        {
+            status = "Walk";
+            my_vector.push_back(status);
+        }
+        else if(time_bike < time_walk)
+        {
+            status = "Bike";
+            my_vector.push_back(status);
+        }
+        else
+        {
+            status = "All";
+            my_vector.push_back(status);
+        }
     }
     
     for(int i = 0; i < my_vector.size();i++)
     {
-        if(my_vector[i] > c )
-        {
-            cout << "Bike" << endl;
-        }
-        else
-        {
-            cout << "Walk" << endl;
-        }
-        
+        cout << my_vector[i]<<endl;
     }
-    
-    
-    
 }
