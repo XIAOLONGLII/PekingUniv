951ed795636c4bf28a1d5eb7d73c7e94e59cba34
XIAOLONG LI <1491skdpf89@gmail.com>
2019-01-02 19:55:28 -0500

    编程题＃1：求字母的个数
    在一个字符串中找出元音字母a,e,i,o,u出现的次数。

--- Week-10/Assignment6-1/main.cpp
+++ Week-10/Assignment6-1/main.cpp
@@ -1,15 +1,32 @@
-//
-//  main.cpp
-//  Assignment6-1
-//
-//  Created by Xiaolong Li on 12/31/18.
-//  Copyright © 2018 Xiaolong Li. All rights reserved.
-//
+//编程题＃1：求字母的个数
 
 #include <iostream>
+using namespace std;
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+   
+//    string s;
+//    getline(cin, s);
+    char s[80];
+    cin.getline(s,80);
+    //cout << s << endl;
+    // a e i o u
+    int Le = 80;
+    int a = 0;
+    int e = 0;
+    int i = 0;
+    int o = 0;
+    int u = 0;
+    int item = 0;
+    while (i != Le)
+    {
+        if(s[item] == 'a') a++;
+        else if(s[item] == 'e') e++;
+        else if (s[item] == 'i') i++;
+        else if (s[item] == 'o') o++;
+        else if (s[item] == 'u') u++;
+        item++;
+        Le--;
+    }
+    cout << a <<" "<<e<<" "<<i<<" "<<o <<" "<<u << endl;
 }
